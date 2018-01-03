b72258eb6d04c69215407330adb13a54dcbd9394
ty <tylergilland@gmail.com>
2018-01-03 01:19:46 -0700

    started search box for existing worktypes

--- clo-workflows/Scripts/src/component/NewProject.tsx
+++ clo-workflows/Scripts/src/component/NewProject.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react'
 import {IUser} from '../model/User';
-import {Work} from './Work';
-import {IArticleWork, WorkType} from '../model/Work';
+import {WorkTypeDropdown} from './WorkTypeDropdown';
+import {IArticleWork, WorkType, IBookWork, IBookChapterWork, IImageWork, IOtherWork, IVideoWork, IWebsiteWork, IMusicWork} from '../model/Work';
 import Header from './Header';
+import {INewProjectState} from '../model/NewProjectState';
+import {UserStore} from '../store/UserStore';
+import {WorkSearch} from "./WorkSearch"
+import {observer} from 'mobx-react';
 interface INewProjectProps{
-  currentUser: IUser
+userStore: UserStore
 }
-const workType: Array<WorkType> = ["Article", "Book", "Book Chapter", "Image", "Music", "Other", "Video", "Website"]
 
-export class NewProject extends React.Component<INewProjectProps> {
-  render(){
+export const NewProject  = observer((props: INewProjectProps)=> {
+    const workTypes: Array<WorkType> = ["Article", "Book", "Book Chapter", "Image", "Music", "Other", "Video", "Website"]
     return(
     <div>
-      <Header currentUser = {this.props.currentUser} />
-      <Work workType={workType}/>  
+      <WorkTypeDropdown workTypes = {workTypes} newProjectState={props.userStore.newProjectState} setNewProjectState={props.userStore.setNewProjectState} /> 
+      {
+        props.userStore.newProjectState.key &&
+        <WorkSearch workType={props.userStore.newProjectState.key}/>
+        
+      } 
     </div>
     )
-  }
-}
+  })
+
