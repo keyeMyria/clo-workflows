790842111dc7c9e032cf04b65759ae6e4becbb34
ty <tylergilland@gmail.com>
2018-01-04 13:27:10 -0700

    added tabs

--- clo-workflows/Scripts/src/component/Anonymous.tsx
+++ clo-workflows/Scripts/src/component/Anonymous.tsx
@@ -8,12 +8,18 @@ import {RootStore} from "../store/RootStore";
 import {UserStore} from "../store/UserStore";
 import {INewProjectState} from "../model/UIStoreInterfaces";
 import Header from "./Header";
-
+import { Label } from 'office-ui-fabric-react/lib/Label';
+import {
+  Pivot,
+  PivotItem,
+  PivotLinkSize
+} from 'office-ui-fabric-react/lib/Pivot';
 interface IAnonymousProps {
 userStore:UserStore
   
 }
 @inject("rootStore")
+@observer
 export class Anonymous extends React.Component<IAnonymousProps, RootStore> {
   componentWillMount() {
     this.userStore = this.props.userStore
@@ -21,13 +27,20 @@ export class Anonymous extends React.Component<IAnonymousProps, RootStore> {
   private userStore: UserStore
   render() {
     console.log("anon component rendered")
-    const { currentUser, currentUserProjects } = this.props.userStore
+    const { currentUser, currentUserProjects, } = this.props.userStore
     return (
       <div>
         <Header currentUser = {this.props.userStore.currentUser} />
-        <NewProject userStore={this.userStore} />
-        <ExistingProjects currentUser={currentUser} currentUserProjects = {currentUserProjects} />
-
+        <Pivot linkSize={ PivotLinkSize.large }>
+          <PivotItem linkText='New Request'>
+            <Label>Create a new Project</Label>
+            <NewProject userStore={this.userStore} />
+          </PivotItem>
+          <PivotItem linkText='Past Requests'>
+            <Label>Your past requests</Label>
+            <ExistingProjects currentUser={currentUser} currentUserProjects = {currentUserProjects} />
+          </PivotItem>
+        </Pivot>
       </div>
     )
   }
